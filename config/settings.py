8a06f9b361b8a07c1cbd294b2b7b2d79805d3818
Sergey <thextreme.30@gmail.com>
2022-09-21 16:57:50 +0600

    channels settings

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'chat',
+
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Channels
+
+ASGI_APPLICATION = 'config.asgi.application'
